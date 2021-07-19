d46131bbb4d0cafea98ccb3a32e9835b0726aa49
LucasLeone <lucasagustinleone2003@gmail.com>
2021-07-19 14:19:53 -0300

    fixed bugs on post apps

---
+++ instagram/posts/apps.py
@@ -0,0 +1,11 @@
+"""Posts app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class PostsAppConfig(AppConfig):
+    """Posts app config."""
+
+    name = 'instagram.posts'
+    verbose_name = 'Posts'
--- instagram/users/models/__init__.py
+++ instagram/users/models/__init__.py
@@ -1,2 +1,2 @@
 from .users import User
-from .profiles import 
+from .profiles import Profile
